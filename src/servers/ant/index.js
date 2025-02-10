dae288a87ccd2c891278c046d3f1f404eb8f96f5
4o4R <120294988+4o4R@users.noreply.github.com>
2025-02-10 17:44:42 -0600

    trying to add speed/cadence to ant+

--- src/servers/ant/index.js
+++ src/servers/ant/index.js
@@ -76,83 +76,103 @@ export class AntServer {
     this.speedCadenceBroadcastInterval.reset();
     this._isRunning = true;
   }
-
-  get isRunning() {
-    return this._isRunning;
-  }
-
-  stop() {
-    const {stick, channel} = this;
-    this.broadcastInterval.cancel();
-    const messages = [
-      Ant.Messages.closeChannel(channel),
-      Ant.Messages.unassignChannel(channel),
-    ];
-    for (let m of messages) {
-      stick.write(m);
-    }
-    this.speedCadenceBroadcastInterval.cancel();
+    export class AntServer {
+      constructor(antStick, options = {}) {
+        const opts = {...defaults, ...options};
+        this.stick = antStick;
+        this.deviceId = opts.deviceId;
     
-    // Speed/cadence channel cleanup
-    const scMessages = [
-      Ant.Messages.closeChannel(SPEED_CADENCE_CHANNEL),
-      Ant.Messages.unassignChannel(SPEED_CADENCE_CHANNEL),
-    ];
+        // Separate channel tracking
+        this.powerChannel = opts.channel;
+        this.speedCadenceChannel = SPEED_CADENCE_CHANNEL;
     
-    for (let m of scMessages) {
-      stick.write(m);
-    }
-  }
-
-  updateMeasurement({ power, cadence }) {
-    this.power = power;
-    this.cadence = cadence;
-  }
-
-  onBroadcastInterval() {
-    const {stick, channel, power, cadence} = this;
-    this.accumulatedPower += power;
-    this.accumulatedPower &= 0xffff;
-    const data = [
-      channel,
-      0x10, // power only
-      this.eventCount,
-      0xff, // pedal power not used
-      cadence,
-      ...Ant.Messages.intToLEHexArray(this.accumulatedPower, 2),
-      ...Ant.Messages.intToLEHexArray(power, 2),
-    ];
-    const message = Ant.Messages.broadcastData(data);
-    debuglog(`ANT+ broadcast power=${power}W cadence=${cadence}rpm accumulatedPower=${this.accumulatedPower}W eventCount=${this.eventCount} message=${message.toString('hex')}`);
-    stick.write(message);
-    this.eventCount++;
-    this.eventCount &= 0xff;
-  }
-
-  onSpeedCadenceBroadcast() {
-    const {stick, cadence} = this;
+        // Power metrics
+        this.eventCount = 0;
+        this.accumulatedPower = 0;
+        this.power = 0;
+        this.cadence = 0;
     
-    // Keiser-specific cadence handling
-    const now = Date.now();
-    if (this.lastCrankRevolutionTime) {
-      const timeDiff = now - this.lastCrankRevolutionTime;
-      const crankRevsDiff = (cadence * timeDiff) / 60000;
-      this.crankRevolutions += Math.round(crankRevsDiff);
-      this.crankEventTime = (this.crankEventTime + timeDiff * 1024/1000) & 0xFFFF;
+        // Speed/Cadence metrics with timestamps
+        this.lastUpdateTime = 0;
+        this.crankRevolutions = 0;
+        this.crankEventTime = 0;
+        this.lastCrankEventTime = 0;
+    
+        // Separate broadcast timers
+        this.powerBroadcastInterval = new Timer(BROADCAST_INTERVAL);
+        this.speedCadenceBroadcastInterval = new Timer(BROADCAST_INTERVAL);
+    
+        this.powerBroadcastInterval.on('timeout', this.onPowerBroadcast.bind(this));
+        this.speedCadenceBroadcastInterval.on('timeout', this.onSpeedCadenceBroadcast.bind(this));
+      }
+
+      get isRunning() {
+        return this._isRunning;
+      }
+
+      stop() {
+        const {stick, powerChannel, speedCadenceChannel} = this;
+        this.powerBroadcastInterval.cancel();
+        this.speedCadenceBroadcastInterval.cancel();
+        const messages = [
+          Ant.Messages.closeChannel(powerChannel),
+          Ant.Messages.unassignChannel(powerChannel),
+          Ant.Messages.closeChannel(speedCadenceChannel),
+          Ant.Messages.unassignChannel(speedCadenceChannel),
+        ];
+        for (let m of messages) {
+          stick.write(m);
+        }
+      }
+
+      updateMeasurement({ power, cadence }) {
+        const now = Date.now();
+        const timeDiff = now - this.lastUpdateTime;
+    
+        // Update power metrics
+        this.power = power;
+        this.cadence = cadence;
+    
+        // Update crank metrics only if cadence changed
+        if (cadence > 0 && timeDiff > 0) {
+          const crankRevsDiff = (cadence * timeDiff) / 60000;
+          this.crankRevolutions += Math.round(crankRevsDiff);
+          this.crankEventTime = (this.crankEventTime + timeDiff * 1024/1000) & 0xFFFF;
+          this.lastCrankEventTime = now;
+        }
+    
+        this.lastUpdateTime = now;
+      }
+
+      onPowerBroadcast() {
+        const data = [
+          this.powerChannel,
+          0x10,
+          this.eventCount,
+          0xff,
+          this.cadence,
+          ...Ant.Messages.intToLEHexArray(this.accumulatedPower, 2),
+          ...Ant.Messages.intToLEHexArray(this.power, 2),
+        ];
+    
+        this.stick.write(Ant.Messages.broadcastData(data));
+        this.accumulatedPower = (this.accumulatedPower + this.power) & 0xffff;
+        this.eventCount = (this.eventCount + 1) & 0xff;
+      }
+
+      onSpeedCadenceBroadcast() {
+        const data = [
+          this.speedCadenceChannel,
+          0x10,
+          ...Ant.Messages.intToLEHexArray(0, 2), // Wheel Revolutions (0 if not used)
+          ...Ant.Messages.intToLEHexArray(0, 2), // Wheel Event Time (0 if not used)
+          this.crankRevolutions & 0xFF,          // Cumulative Crank Revolutions (8-bit)
+          ...Ant.Messages.intToLEHexArray(this.crankEventTime, 2)  // Last Crank Event Time (16-bit LE)
+        ];
+        
+        const message = Ant.Messages.broadcastData(data);
+        debuglog(`ANT+ SC broadcast: cranks=${this.crankRevolutions} time=${this.crankEventTime}`);
+        this.stick.write(message);
+      }
+      
     }
-    this.lastCrankRevolutionTime = now;
-
-    const data = [
-      SPEED_CADENCE_CHANNEL,
-      0x10,
-      ...Ant.Messages.intToLEHexArray(this.wheelRevolutions, 2),
-      ...Ant.Messages.intToLEHexArray(this.wheelEventTime, 2),
-      this.crankRevolutions & 0xFF,
-      ...Ant.Messages.intToLEHexArray(this.crankEventTime, 2)
-    ];
-
-    const message = Ant.Messages.broadcastData(data);
-    debuglog(`ANT+ SC broadcast (Keiser): cranks=${this.crankRevolutions} time=${this.crankEventTime}`);
-    stick.write(message);
-  }
-}
